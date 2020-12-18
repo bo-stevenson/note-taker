42478d1ab4453f7ef629b5c1da71f38d76a9fbe1
Bo Stevenson <bostevenson1027@gmail.com>
2020-12-18 13:23:33 -0600

    Added require dependencies and .gitignore

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const directory = path.join(__dirname, "/public");
+const database = path.join(__dirname, "/db/db.json");
