585fd0275b5d5d18e24c3714930479941b7f5580
Bo Stevenson <bostevenson1027@gmail.com>
2020-12-18 13:59:47 -0600

    added html routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Require dependencies 
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -7,3 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 const directory = path.join(__dirname, "/public");
 const database = path.join(__dirname, "/db/db.json");
+
+
+//Server HTML Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(directory, "notes.html"))
+});
